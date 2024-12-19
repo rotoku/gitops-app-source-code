2cac0c2ad9d0ef979c2791e214dbb589c97f1e59
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2024-12-19 14:22:51 -0300

    feat: first commit

---
+++ app/src/main/java/br/com/kumabe/cocoladora/CocoladoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.kumabe.cocoladora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoladoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocoladoraApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/br/com/kumabe/cocoladora/CocoladoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kumabe.cocoladora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoladoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
